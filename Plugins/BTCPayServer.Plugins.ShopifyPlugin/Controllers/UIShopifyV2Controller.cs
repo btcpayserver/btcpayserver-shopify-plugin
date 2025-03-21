8d1bf4b91036002270f58745858da4c5216e5730
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-03-21 12:40:42 +0100

    default redirect value

--- Plugins/BTCPayServer.Plugins.ShopifyPlugin/Controllers/UIShopifyV2Controller.cs
+++ Plugins/BTCPayServer.Plugins.ShopifyPlugin/Controllers/UIShopifyV2Controller.cs
@@ -240,7 +240,7 @@ public class UIShopifyV2Controller : Controller
     [AllowAnonymous]
     [EnableCors(CorsPolicies.All)]
     [HttpGet("~/stores/{storeId}/plugins/shopify-v2/checkout")]
-    public async Task<IActionResult> Checkout(string storeId, string? checkout_token, bool redirect, CancellationToken cancellationToken)
+    public async Task<IActionResult> Checkout(string storeId, string? checkout_token, bool redirect = true, CancellationToken cancellationToken)
     {
         if (checkout_token is null)
             return BadRequest("Invalid checkout token");
