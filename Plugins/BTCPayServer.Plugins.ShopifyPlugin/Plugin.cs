431d4c985efa18c755430d4b79749369ffd19ded
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-01-16 13:10:18 +0100

    include shopify refund to email rule

--- Plugins/BTCPayServer.Plugins.ShopifyPlugin/Plugin.cs
+++ Plugins/BTCPayServer.Plugins.ShopifyPlugin/Plugin.cs
@@ -1,5 +1,7 @@
 using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Models;
+using BTCPayServer.Plugins.Emails;
+using BTCPayServer.Plugins.Emails.Views;
 using BTCPayServer.Plugins.ShopifyPlugin.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,8 +17,41 @@ public class Plugin : BaseBTCPayServerPlugin
     public override void Execute(IServiceCollection services)
     {
         services.AddUIExtension("header-nav", "ShopifyPluginHeaderNav");
-        services.AddSingleton<EmailService>();
         services.AddSingleton<ShopifyClientFactory>();
         services.AddHostedService<ShopifyHostedService>();
+        RegisterShopifyEmailTriggers(services);
     }
+
+
+    private void RegisterShopifyEmailTriggers(IServiceCollection services)
+    {
+        var vm = new EmailTriggerViewModel()
+        {
+            Trigger = ShopifyMailTriggers.RefundCreated,
+            DefaultEmail = new()
+            {
+                To = ["{Customer.Email}"],
+                Subject = "Refund for Shopify Order #{Order.Id}",
+                Body = EmailsPlugin.CreateEmail(
+                    "Hello {Customer.Name},<br/><br/>A refund has been created for your shopify order #{Order.Id}.<br/><br/>You can claim your refund using the link below.",
+                    "Claim Refund",
+                    "{RefundLink}"
+                ),
+            },
+            PlaceHolders = new()
+            {
+                new("{RefundLink}", "The link to claim the refund"),
+                new("{Order.Id}", "The Shopify order ID"),
+                new("{Customer.Email}", "The customer's email address"),
+                new("{Customer.Name}", "The customer's name"),
+            },
+            Description = "Shopify: Refund Created",
+        };
+        services.AddSingleton(vm);
+    }
+}
+
+public static class ShopifyMailTriggers
+{
+    public const string RefundCreated = "SRV-ShopifyRefundCreated";
 }
