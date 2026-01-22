bcd761ac238a3e5ddac07b99e8a9382b4f95f0ca
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-01-22 09:54:39 +0100

    make current exchange rate default

--- Plugins/BTCPayServer.Plugins.ShopifyPlugin/ViewModels/ShopifyRefundWebhookSettingsViewModel.cs
+++ Plugins/BTCPayServer.Plugins.ShopifyPlugin/ViewModels/ShopifyRefundWebhookSettingsViewModel.cs
@@ -7,15 +7,15 @@ public class ShopifyRefundWebhookSettingsViewModel
 {
     public enum RefundOption
     {
-        RateThen,
-        CurrentRate
+        CurrentRate,
+        RateThen
     }
 
     public string WebhookUrl { get; set; }
     [Required(ErrorMessage = "Webhook secret is required")]
     public string WebhookSecret { get; set; }
     [Required(ErrorMessage = "Please select a refund option")]
-    public RefundOption? SelectedRefundOption { get; set; }
+    public RefundOption SelectedRefundOption { get; set; } = RefundOption.CurrentRate;
 
     [Range(0, 99, ErrorMessage = "Spread must be between 0 and 99")]
     public decimal SpreadPercentage { get; set; } = 0;
