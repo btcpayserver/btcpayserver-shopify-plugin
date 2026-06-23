d8e3334730ea1478f0ea5964501ecd714aefdf01
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-06-23 08:11:00 +0100

    fix coderabbit

--- Plugins/BTCPayServer.Plugins.ShopifyPlugin/Services/AsyncDuplicateLock.cs
+++ Plugins/BTCPayServer.Plugins.ShopifyPlugin/Services/AsyncDuplicateLock.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,23 +7,71 @@ namespace BTCPayServer.Plugins.ShopifyPlugin.Services;
 
 public sealed class AsyncDuplicateLock
 {
-    private readonly ConcurrentDictionary<string, SemaphoreSlim> _locks = new();
+    private sealed class LockRef
+    {
+        public readonly SemaphoreSlim Semaphore = new(1, 1);
+        public int RefCount;
+    }
+
+    private readonly Dictionary<string, LockRef> _locks = new();
+    private readonly object _gate = new();
 
     public async Task<IDisposable> LockAsync(string key, CancellationToken ct = default)
     {
-        var sem = _locks.GetOrAdd(key, _ => new SemaphoreSlim(1, 1));
-        await sem.WaitAsync(ct);
-        return new Releaser(this, key, sem);
+        LockRef lockRef;
+        lock (_gate)
+        {
+            if (!_locks.TryGetValue(key, out lockRef!))
+            {
+                lockRef = new LockRef();
+                _locks[key] = lockRef;
+            }
+            lockRef.RefCount++;
+        }
+        try
+        {
+            await lockRef.Semaphore.WaitAsync(ct);
+        }
+        catch
+        {
+            Release(key, lockRef, releaseSemaphore: false);
+            throw;
+        }
+        return new Releaser(this, key, lockRef);
+    }
+
+    private void Release(string key, LockRef lockRef, bool releaseSemaphore)
+    {
+        lock (_gate)
+        {
+            if (releaseSemaphore)
+                lockRef.Semaphore.Release();
+
+            lockRef.RefCount--;
+            if (lockRef.RefCount == 0)
+            {
+                _locks.Remove(key);
+                lockRef.Semaphore.Dispose();
+            }
+        }
     }
 
     private sealed class Releaser : IDisposable
     {
         private readonly AsyncDuplicateLock _parent;
         private readonly string _key;
-        private readonly SemaphoreSlim _sem;
-        public Releaser(AsyncDuplicateLock parent, string key, SemaphoreSlim sem)
-            => (_parent, _key, _sem) = (parent, key, sem);
+        private readonly LockRef _lockRef;
+        private int _disposed;
 
-        public void Dispose() => _sem.Release();
+        public Releaser(AsyncDuplicateLock parent, string key, LockRef lockRef)
+            => (_parent, _key, _lockRef) = (parent, key, lockRef);
+
+        public void Dispose()
+        {
+            if (Interlocked.Exchange(ref _disposed, 1) == 0)
+                _parent.Release(_key, _lockRef, releaseSemaphore: true);
+        }
     }
 }
+
+
